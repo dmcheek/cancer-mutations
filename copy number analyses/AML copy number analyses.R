008ef383ba7defe141ede56866ff2f2d1e605f50
dmcheek <133163574+dmcheek@users.noreply.github.com>
2025-03-06 21:01:18 -0500

    Update AML copy number analyses.R

--- copy number analyses/AML copy number analyses.R
+++ copy number analyses/AML copy number analyses.R
@@ -40,8 +40,6 @@ AMLCHff$mutation_frequency_rank<-(rank(AMLCHff$AML_freq)+rank(AMLCHff$CH_freq))/
 
 AMLCHfff<-AMLCHff%>%slice_max(order_by=mutation_frequency_rank,n=20)
 
-cor.test(AMLCHfff$mean_age,AMLCHfff$AML_freq,method="spearman")
-
 wtd.cor(rank(AMLCHfff$CH_freq),rank(AMLCHfff$mean_age),weight=AMLCHfff$AML_freq)
 spCH<-cor.test(AMLCHfff$CH_freq,AMLCHfff$mean_age,method="spearman")
 ggplot(filter(AMLCHfff),aes(x=CH_freq,y=mean_age,label=annotation))+geom_smooth(method="lm",se=F,colour=viridis(n=1,begin=0.8,end=0.8,option="mako",alpha=0.5))+geom_text_repel()+geom_point()+theme_classic()+guides(size="none")+labs()+labs(x="frequency in normal blood",y="mean age in AML",colour="frequency\nin AML",caption=paste("rho=",unname(round(spCH$estimate,2)),", p=",signif(spCH$p.value,2),sep=""))+scale_colour_viridis(end=0.95,option="F",trans="log10",direction=-1)+scale_x_continuous(trans="sqrt",breaks=c(0,0.0001,0.0004,0.001))#+scale_y_continuous(trans="sqrt",breaks=c(0,0.01,0.04,0.1))
@@ -50,6 +48,6 @@ wtd.cor(rank(AMLCHfff$OR),rank(AMLCHfff$mean_age),weight=AMLCHfff$AML_freq)
 spOR<-cor.test(AMLCHfff$OR,AMLCHfff$mean_age,method="spearman")
 ggplot(AMLCHfff,aes(x=OR,y=mean_age,label=annotation))+geom_smooth(method="lm",se=F,colour=viridis(n=1,begin=0.8,end=0.8,option="mako",alpha=0.5))+geom_text_repel()+geom_point()+scale_shape_manual()+theme_classic()+guides(size="none")+labs()+labs(x="estimated carcinogenic effect",y="mean age in AML",colour="frequency\nin AML",caption=paste("rho=",unname(round(spOR$estimate,2)),", p=",signif(spOR$p.value,2),sep=""))+scale_colour_viridis(end=0.95,option="F",trans="log10",direction=-1)+scale_x_continuous(trans="log10",breaks=10^(1:5))
 
-filtparplotw<-read_csv("AML SCNA filtering parameters.csv")
+filtparplotw<-read_csv("AML filtering parameters.csv")
 ggplot(filtparplotw,aes(x=upperCNA,y=topalts,fill=rho))+facet_wrap(~CHOR)+geom_tile()+scale_fill_gradient2(mid="white",high=viridis(n=1,begin=0.8,end=0.8,option="G"),low=viridis(n=1,begin=0.2,end=0.2,option="G"),midpoint=0)+theme_classic()+labs(x="upper bound on number of SCNAs per sample",y="top n most common alterations")
 
